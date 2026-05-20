6b61d3a653eed82fec2f6138a84d56d108a4d690
miscellaneous-mice <prateeknarasimha@gmail.com>
2026-05-21 01:23:24 +0530

    Kalman filter implementation

--- 08_Kalman_FIlter/Qsys/software/kalman_filter/obj/default/main.d
+++ 08_Kalman_FIlter/Qsys/software/kalman_filter/obj/default/main.d
@@ -1,6 +1,5 @@
-obj/default/hello_world_small.o: hello_world_small.c \
- ../kalman_filter_bsp/system.h ../kalman_filter_bsp/linker.h \
- ../kalman_filter_bsp//HAL/inc/io.h \
+obj/default/main.o: main.c ../kalman_filter_bsp/system.h \
+ ../kalman_filter_bsp/linker.h ../kalman_filter_bsp//HAL/inc/io.h \
  ../kalman_filter_bsp//HAL/inc/alt_types.h
 
 ../kalman_filter_bsp/system.h:
